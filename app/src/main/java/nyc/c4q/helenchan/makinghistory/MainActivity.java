d92577ae7130e2c814976c3b94396c679b23845e
Helen Chan <hchan22@gmail.com>
2017-02-27 15:44:02 -0500

    db

--- app/src/main/java/nyc/c4q/helenchan/makinghistory/MainActivity.java
+++ app/src/main/java/nyc/c4q/helenchan/makinghistory/MainActivity.java
@@ -3,11 +3,18 @@ package nyc.c4q.helenchan.makinghistory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mFirebasseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
     }
 }
