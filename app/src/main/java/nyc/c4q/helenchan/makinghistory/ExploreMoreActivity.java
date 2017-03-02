93fd68ec04a223d11365e72f50086883f6a66e53
Akasha Archer <Akasha@LLD-IMAC.home>
2017-03-02 14:39:25 -0500

    User can enter and search for location on map

--- app/src/main/java/nyc/c4q/helenchan/makinghistory/ExploreMoreActivity.java
+++ app/src/main/java/nyc/c4q/helenchan/makinghistory/ExploreMoreActivity.java
@@ -1,6 +1,8 @@
 package nyc.c4q.helenchan.makinghistory;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,6 +10,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -15,6 +22,7 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -25,7 +33,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 public class ExploreMoreActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
@@ -37,6 +47,8 @@ public class ExploreMoreActivity extends AppCompatActivity implements OnMapReady
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
     private GoogleApiClient mLocationClient;
+    private float zoomLevel = 15;
+
 
     private FirebaseDatabase mFirebaseDatabase;
     private FirebaseAuth mFirebaseAuth;
@@ -66,8 +78,19 @@ public class ExploreMoreActivity extends AppCompatActivity implements OnMapReady
                     .build();
 
             mLocationClient.connect();
-
         }
+
+        Button searchAddressBtn = (Button) findViewById(R.id.location_search_btn);
+        searchAddressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    locateFromAddress(view);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     private void setAuthenticationListener() {
@@ -118,6 +141,12 @@ public class ExploreMoreActivity extends AppCompatActivity implements OnMapReady
         return true;
     }
 
+    private void gotoLocation(double lat, double lng, float zoom) {
+        LatLng latlng = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng, zoom);
+        mMap.moveCamera(update);
+    }
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -127,9 +156,34 @@ public class ExploreMoreActivity extends AppCompatActivity implements OnMapReady
         mMap.moveCamera(CameraUpdateFactory.newLatLng(googleHQ));
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 5000, null);
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(zoomLevel), 5000, null);
     }
 
+
+    private void hideSoftKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    public void locateFromAddress(View v) throws IOException {
+        hideSoftKeyboard(v);
+
+        EditText locationInput = (EditText) findViewById(R.id.location_search_input);
+        String searchString = locationInput.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(searchString, 1);
+
+        if (list.size() > 0) {
+            Address add = list.get(0);
+            String locality = add.getLocality();
+            Toast.makeText(this, "Found: " + locality, Toast.LENGTH_SHORT).show();
+
+            double lat = add.getLatitude();
+            double lng = add.getLongitude();
+            gotoLocation(lat, lng, zoomLevel);
+        }
+    }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
