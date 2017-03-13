f3b92dc810ecf844e74b8a50b7ed46e3500dcb82
Helen Chan <hchan22@gmail.com>
2017-03-13 18:39:35 -0400

    saving text from userstory edittext

--- app/src/main/java/nyc/c4q/helenchan/makinghistory/EditContentActivity.java
+++ app/src/main/java/nyc/c4q/helenchan/makinghistory/EditContentActivity.java
@@ -2,17 +2,15 @@ package nyc.c4q.helenchan.makinghistory;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -26,8 +24,6 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.IOException;
-
 import nyc.c4q.helenchan.makinghistory.models.Content;
 
 /**
@@ -36,9 +32,11 @@ import nyc.c4q.helenchan.makinghistory.models.Content;
 
 public class EditContentActivity extends AppCompatActivity {
     private ImageView portraitIV;
+    private EditText storyEditText;
     private Uri photoUri;
     private Uri downloadUri;
     private String userLocationKey;
+    private String userStory;
 
     private DatabaseReference mFirebaseDatabase;
     private FirebaseStorage mFirebaseStorage;
@@ -74,6 +72,7 @@ public class EditContentActivity extends AppCompatActivity {
         mFirebaseStorage = FirebaseStorage.getInstance();
         myStorageRef = mFirebaseStorage.getReference();
         portraitIV = (ImageView) findViewById(R.id.preview_portrait_iv);
+        storyEditText = (EditText) findViewById(R.id.user_story_edittext);
         mProgressDialog = new ProgressDialog(EditContentActivity.this);
     }
 
@@ -92,11 +91,24 @@ public class EditContentActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case(R.id.edit_content_save):
-
-                uploadingToFireBase();
-                return true;
+                if(checkifEdittextHasText()){
+                    userStory = String.valueOf(storyEditText.getText());
+                    uploadingToFireBase();
+                    return true;
+                }
+                return false;
             }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private Boolean checkifEdittextHasText(){
+        if(storyEditText.length() == 0){
+            Toast.makeText(getApplicationContext(), "Please enter a short description of photo", Toast.LENGTH_SHORT).show();
+        return false;
+        }else {
+            return true;
+        }
     }
 
     private void uploadingToFireBase( ) {
@@ -111,7 +123,7 @@ public class EditContentActivity extends AppCompatActivity {
                 mProgressDialog.dismiss();
                 Toast.makeText(getApplicationContext(), "Uploaded", Toast.LENGTH_LONG).show();
                 downloadUri = taskSnapshot.getDownloadUrl();
-                addUserContentToDatabase(userLocationKey, downloadUri.toString());
+                addUserContentToDatabase(userLocationKey, downloadUri.toString(), userStory);
                 returnToMap();
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -122,9 +134,9 @@ public class EditContentActivity extends AppCompatActivity {
         });
     }
 
-    private void addUserContentToDatabase(String userLocationKey, String url) {
+    private void addUserContentToDatabase(String userLocationKey, String url, String story) {
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        mFirebaseDatabase.child("MapPoint").child(userLocationKey).child("ContentList").push().setValue(new Content(" ", "Akasha testing", " ", "wash sq", url, "2017"));
+        mFirebaseDatabase.child("MapPoint").child(userLocationKey).child("ContentList").push().setValue(new Content(" ", "story", story, "wash sq", url, "2017"));
         mFirebaseDatabase.child("Users").child(uid).child("ContentList").push().setValue(new Content(" ", uid, " ", "wash sq", url, "2017"));
     }
 
