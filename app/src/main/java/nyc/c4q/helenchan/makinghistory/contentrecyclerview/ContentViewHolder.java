5e8cb543376e316f1f1b2e2365445dc6cb9e5c4e
Shannon Alexander-Navarro <shannonalexander@hotmail.com>
2017-03-16 21:18:21 -0400

    pic onclick until animation works

--- app/src/main/java/nyc/c4q/helenchan/makinghistory/contentrecyclerview/ContentViewHolder.java
+++ app/src/main/java/nyc/c4q/helenchan/makinghistory/contentrecyclerview/ContentViewHolder.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.easyandroidanimations.library.TransferAnimation;
 
 import nyc.c4q.helenchan.makinghistory.R;
 import nyc.c4q.helenchan.makinghistory.models.Content;
@@ -49,33 +48,33 @@ public class ContentViewHolder extends RecyclerView.ViewHolder {
         contentDescriptionTextView.setText(c.getText());
         contentYearTextView.setText(c.getYear());
 
-//        contentImageView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                contentImageView.setVisibility(View.INVISIBLE);
-//                expandedImage.setVisibility(View.VISIBLE);
-//                Glide.with(itemView.getContext())
-//                        .load(c.getUrl())
-//                        .centerCrop()
-//                        .diskCacheStrategy(DiskCacheStrategy.SOURCE)
-//                        .into(expandedImage);
-//            }
-//        });
-//
-//        expandedImage.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                expandedImage.setVisibility(View.INVISIBLE);
-//                contentImageView.setVisibility(View.VISIBLE);
-//            }
-//        });
-
         contentImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new TransferAnimation(contentImageView).setDestinationView(expandedImage).animate();
+                contentImageView.setVisibility(View.INVISIBLE);
+                expandedImage.setVisibility(View.VISIBLE);
+                Glide.with(itemView.getContext())
+                        .load(c.getUrl())
+                        .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                        .into(expandedImage);
             }
         });
+
+        expandedImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                expandedImage.setVisibility(View.INVISIBLE);
+                contentImageView.setVisibility(View.VISIBLE);
+            }
+        });
+
+//        contentImageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                new TransferAnimation(contentImageView).setDestinationView(expandedImage).animate();
+//            }
+//        });
     }
 
 }
